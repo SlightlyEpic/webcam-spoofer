bb7f8c98114d519b88f6606ed19904e4f44a675f
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2023-11-09 19:44:29 +0530

    background script and keyauth

---
+++ background/auth.js
@@ -0,0 +1,173 @@
+const APP_NAME = 'Verification Bot';
+const OWNER_ID = 'QUwMZM0GVr';
+
+let isInitialized = false;
+let activeUser = null;
+let activeSessionId = null;
+let activeLicenseKey = null;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function tryInitialize() {
+    return new Promise((resolve, reject) => {
+        if(isInitialized) resolve(activeSessionId);
+
+        if(!APP_NAME) reject('APP_NAME is not set');
+        if(!OWNER_ID) reject('OWNER_ID is not set');
+
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Host: 'keyauth.win',
+                'content-type': 'application/x-www-form-urlencoded',
+            }
+        };
+
+        const params = {
+            type: 'init',
+            ver: '1.0',
+            name: APP_NAME,
+            ownerid: OWNER_ID
+        };
+
+        fetch('https://keyauth.win/api/1.2/?' + new URLSearchParams(params), options)
+        .then(res => res.json())
+        .then(res => {
+            if(!res.success) reject(res.message);
+
+            isInitialized = true;
+            activeSessionId = res.sessionid;
+            resolve(res.sessionid);
+        })
+        .catch(reject);
+
+        // fetch('https://random-data-api.com/api/v2/users?size=2&response_type=json')
+        // .then(res => res.json())
+        // .then(console.log)
+        // .catch(console.error);
+    });
+}
+
+function tryAutoVerify() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(['verifyKey'], result => {
+            if(chrome.runtime.lastError) reject(chrome.runtime.lastError);
+
+            if(!('verifyKey' in result)) reject('Verify key not in storage');
+
+            tryInitialize()
+            .then(() => tryVerify(result.verifyKey))
+            .then(resolve)
+            .catch(reject);
+        });
+    });
+}
+
+function tryVerify(licenseKey) {
+    return new Promise((resolve, reject) => {
+        if(!licenseKey) reject('licenseKey is invalid');
+        if(!activeSessionId) reject('activeSessionId is not set');
+        if(!APP_NAME) reject('APP_NAME is not set');
+        if(!OWNER_ID) reject('OWNER_ID is not set');
+        
+        const options = {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                Host: 'keyauth.win',
+                'content-type': 'application/x-www-form-urlencoded',
+            }
+        };
+
+        const params = {
+            type: 'license',
+            key: licenseKey,
+            sessionid: activeSessionId,
+            name: APP_NAME,
+            ownerid: OWNER_ID
+        }
+          
+        fetch('https://keyauth.win/api/1.2/?' + new URLSearchParams(params), options)
+        .then(res => {
+            console.warn('aaa ', res);
+            return res;
+        })
+        .then(res => res.json())
+        .then(res => {
+            if(!response.success) reject(res.message);
+
+            activeLicenseKey = licenseKey;
+            activeUser = res.info;
+            resolve(res.info);
+        })
+        .catch(reject);
+    });
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+chrome.runtime.onMessage.addListener((req, sender, _sendRes) => {
+    console.log(`Message: ${req.message}\n`, req);
+
+    const sendRes = (...args) => {
+        console.log(`Response: `, ...args);
+        _sendRes(...args);
+    };
+
+    switch(req.message) {
+        case 'autoVerify':
+            tryAutoVerify()
+            .then(() => {
+                sendRes({
+                    success: true,
+                    message: 'Verified!'
+                });
+            })
+            .catch(err => {
+                sendRes({
+                    success: false,
+                    error: err
+                });
+            });
+            break;
+
+        case 'verify':
+            tryInitialize()
+            .then(() => tryVerify(req.licenseKey))
+            .then(() => {
+                chrome.storage.local.set({ verifyKey: req.licenseKey }, () => {
+                    if(chrome.runtime.lastError) console.error(chrome.runtime.lastError);
+
+                    sendRes({
+                        success: true,
+                        message: 'Verified!'
+                    });
+                });
+            })
+            .catch(err => {
+                sendRes({
+                    success: false,
+                    error: err
+                });
+            });
+            break;
+        
+        case 'isVerified':
+            sendRes({ 
+                success: true, 
+                verified: (activeLicenseKey != null), 
+                user: activeUser 
+            });
+            break;
+        
+        default:
+            sendRes({ 
+                success: false, 
+                error: 'Unknown message' 
+            });
+            break;
+    }
+});
+
+console.log('Hello');
---
+++ popup/verify.js
@@ -0,0 +1,63 @@
+document.addEventListener('load', () => {
+    // Send isVerified message to auth.js
+    chrome.runtime.sendMessage({ message: 'isVerified' }, res => {
+        if(!res.success) return statusError(res.error);
+        if(res.verified) showMain();
+
+        statusWarn('Trying to auto verify...');
+        chrome.runtime.sendMessage({ message: 'autoVerify' }, res => {
+            if(!res.success) return statusError(res.error);
+            
+            statusSuccess('Verified!');
+            showMain();
+        });
+    })
+});
+
+document.getElementById('verifyButton').addEventListener('click', () => {
+    let key = document.getElementById('keyInput').value;
+    let status = document.getElementById('status');
+
+    if(key) {
+        statusWarn('Verifying...');
+
+        // Send verify message to auth.js
+        chrome.runtime.sendMessage({ message: 'verify', licenseKey: key }, res => {
+            if(!res.success) return statusError(res.error);
+
+            statusSuccess('Verified!');
+            showMain();
+        })
+    } else {
+        statusError('Please enter a key');
+        console.warn(status.innerText);
+    }
+});
+
+function statusError(err) {
+    let status = document.getElementById('status');
+    status.style.color = 'red';
+    status.innerText = err;
+}
+
+function statusWarn(msg) {
+    let status = document.getElementById('status');
+    status.style.color = 'yellow';
+    status.innerText = msg;
+}
+
+function statusSuccess(msg) {
+    let status = document.getElementById('status');
+    status.style.color = 'green';
+    status.innerText = msg;
+}
+
+function showVerify() {
+    document.getElementById('verifyContainer').style.display = 'block';
+    document.getElementById('mainContainer').style.display = 'none';
+}
+
+function showMain() {
+    document.getElementById('verifyContainer').style.display = 'none';
+    document.getElementById('mainContainer').style.display = 'block';
+}
